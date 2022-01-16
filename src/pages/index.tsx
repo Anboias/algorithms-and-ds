c37b3807ef7514a797fb531eee9d21b6a5144af4
Anboias <biasinovschi@gmail.com>
2022-01-16 03:25:38 +0200

    Updated first page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,17 +8,37 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+      }}
+    >
+      <h1>Hello, world.</h1>
+      <h4 style={{ color: "gray" }}>
+        I'm{" "}
+        <Link
+          style={{ textDecoration: "none" }}
+          to="https://biasinov.com"
+          target={"_blank"}
+        >
+          Bogdan
+        </Link>
+        , a Full Stack Web developer based in Bucharest, Romania.
+      </h4>
+      <p>Welcome to my visualization project!</p>
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={300}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+    </div>
   </Layout>
 )
 
