16452bfd88144f3c712de0be1ba633ab69906769
Anboias <biasinovschi@gmail.com>
2022-01-16 16:42:24 +0200

    Refactoring

--- src/components/sorting/bubblesort.tsx
+++ src/components/sorting/bubblesort.tsx
@@ -5,106 +5,28 @@ import constants from "../../constants"
 
 interface BubbleSortI {
   data?: number[]
-  speed: number
+  sortedValues: Set<unknown>
+  twoNumbersSelection: any
 }
 
-const BubbleSort = ({ data, speed }: BubbleSortI) => {
-  // Store values locally and changed them while sorting
-  const [localData, setLocalData] = React.useState(null)
-
+const BubbleSort = ({
+  data,
+  sortedValues,
+  twoNumbersSelection,
+}: BubbleSortI) => {
   // Constants
   const [max, min] = React.useMemo(
-    () => [Math.max(...data), Math.min(...data)],
+    () => (data ? [Math.max(...data), Math.min(...data)] : null),
     [data]
   )
   const calcWidth = 100 / data.length
-  const twoNumbersSelection = React.useRef([])
-  const sortedValues = React.useMemo(() => new Set(), [data])
 
-  // Do sort
-  const sortingSteps = React.useMemo(
-    () => algos.bubbleSortAlgorithm(data),
-    [data]
-  )
-
-  const doDelay = (delay: number) =>
-    new Promise<void>((resolve) => setTimeout(() => resolve(), delay))
-
-  const doMagic = React.useCallback(async () => {
-    for (let orderEl of sortingSteps) {
-      const [firstEl, secondEl, swap, sortedValue]: number[] = orderEl
-
-      // setTwoNumbersSelection([firstEl, secondEl])
-
-      if (sortedValue) {
-        sortedValues.add(sortedValue)
-      }
-
-      // console.log("before delay", speed)
-
-      // Add delay to the execution
-      await doDelay(speed)
-
-      // console.log("after delay", speed)
-      // console.log("====================")
-
-      setLocalData((prevValues: number[]) => {
-        const indexOfFirst = prevValues.indexOf(firstEl)
-        const indexOfSecond = prevValues.indexOf(secondEl)
-
-        if (swap) {
-          prevValues[indexOfFirst] = secondEl
-          prevValues[indexOfSecond] = firstEl
-        }
-
-        const containerWidth =
-          document.getElementById("bars-container").clientWidth
-
-        const offset = containerWidth / data.length
-
-        if (firstEl && secondEl) {
-          // Slide first bar
-          const leftEl = document.getElementById(`bar-${firstEl}`)
-          if (leftEl)
-            leftEl.style.transform = `translate(${
-              firstEl > secondEl ? offset : 0
-            }px,0)`
-
-          // Slide second bar
-          const rightEl = document.getElementById(`bar-${secondEl}`)
-          if (rightEl)
-            rightEl.style.transform = `translate(${
-              secondEl > firstEl ? 0 : -offset
-            }px,0)`
-        }
-
-        twoNumbersSelection.current = [
-          Math.min(firstEl, secondEl),
-          Math.max(firstEl, secondEl),
-        ]
-        // twoNumbersSelection.current[1] = secondEl
-        return prevValues.slice()
-      })
-    }
-    // Set local data to trigger a final render
-    setLocalData((prevValues: number[]) => [...prevValues])
-    // Add the remaining elements to the values sorted array
-    sortingSteps.forEach((el) => sortedValues.add(el))
-  }, [speed])
-
-  // Update UI for each sorting step
-  React.useEffect(() => {
-    doMagic()
-  }, [sortingSteps])
-
-  // Initialize vars
-  React.useEffect(() => {
-    setLocalData(data)
-    sortedValues.clear()
-    twoNumbersSelection.current = []
-  }, [data])
-
-  // console.log("test")
+  // // Initialize vars
+  // React.useEffect(() => {
+  //   setLocalData(data)
+  //   sortedValues.clear()
+  //   twoNumbersSelection.current = []
+  // }, [data])
 
   return (
     <div
@@ -116,53 +38,57 @@ const BubbleSort = ({ data, speed }: BubbleSortI) => {
         // transition: "1s all ease",
       }}
     >
-      {localData?.map((item: number, idx: number) => {
-        const calcHeight = ((max - min) / 100) * item
+      {data ? (
+        data.map((item: number, idx: number) => {
+          const calcHeight = ((max - min) / 100) * item
 
-        return (
-          <div
-            id={`bar-${item}`}
-            key={`${idx}-${item}`}
-            style={{
-              height: `calc(${calcHeight}% - 60px)`,
-              minHeight: `30px`,
-              width: `calc(${calcWidth}% - .3em)`,
-              backgroundColor: sortedValues.has(item)
-                ? constants.COLORS.DARK_GREEN
-                : twoNumbersSelection.current[0] === item
-                ? constants.COLORS.LEFT_BAR
-                : twoNumbersSelection.current[1] === item
-                ? constants.COLORS.RIGHT_BAR
-                : constants.COLORS.CURRENT,
-              margin: `0 .15em`, // Change logic to fit in 100%
-              display: `flex`,
-              justifyContent: `center`,
-              borderRadius: "0 0 5px 5px",
-              transition: "all 0.2s ease-in-out",
-              "webkit-transition": "all 0.2s ease-in-out",
-              "-moz-transition": "all 0.2s ease-in-out",
-              "-o-transition": "all 0.2s ease-in-out",
-              "-ms-transition": "all 0.2s ease-in-out",
-            }}
-          >
-            <p
+          return (
+            <div
+              id={`bar-${item}`}
+              key={`${idx}-${item}`}
               style={{
-                color: sortedValues.has(item)
-                  ? "white"
+                height: `calc(${calcHeight}% - 60px)`,
+                minHeight: `30px`,
+                width: `calc(${calcWidth}% - .3em)`,
+                backgroundColor: sortedValues.has(item)
+                  ? constants.COLORS.DARK_GREEN
                   : twoNumbersSelection.current[0] === item
-                  ? "white"
+                  ? constants.COLORS.LEFT_BAR
                   : twoNumbersSelection.current[1] === item
-                  ? "white"
-                  : constants.COLORS.DARK_GREEN,
-                fontSize: "0.7em",
-                padding: 5,
+                  ? constants.COLORS.RIGHT_BAR
+                  : constants.COLORS.CURRENT,
+                margin: `0 .15em`, // Change logic to fit in 100%
+                display: `flex`,
+                justifyContent: `center`,
+                borderRadius: "0 0 5px 5px",
+                transition: "all 0.2s ease-in-out",
+                "webkit-transition": "all 0.2s ease-in-out",
+                "-moz-transition": "all 0.2s ease-in-out",
+                "-o-transition": "all 0.2s ease-in-out",
+                "-ms-transition": "all 0.2s ease-in-out",
               }}
             >
-              {item}
-            </p>
-          </div>
-        )
-      })}
+              <p
+                style={{
+                  color: sortedValues.has(item)
+                    ? "white"
+                    : twoNumbersSelection.current[0] === item
+                    ? "white"
+                    : twoNumbersSelection.current[1] === item
+                    ? "white"
+                    : constants.COLORS.DARK_GREEN,
+                  fontSize: "0.7em",
+                  padding: 5,
+                }}
+              >
+                {data?.length <= 50 ? item : ""}{" "}
+              </p>
+            </div>
+          )
+        })
+      ) : (
+        <h1>Error</h1>
+      )}
     </div>
   )
 }
--- src/pages/sortingalgorithms.tsx
+++ src/pages/sortingalgorithms.tsx
@@ -5,10 +5,17 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Content from "../components/content"
-import BubbleSort from "../components/sorting/bubblesort"
+import SortingTable from "../components/sorting/bubblesort"
 
-import Box from "@mui/material/Box"
-import Slider from "@mui/material/Slider"
+import { algos } from "../components/sorting/algorithms/sorting"
+
+import { Box, Button, Slider } from "@mui/material"
+import FastForwardIcon from "@mui/icons-material/FastForward"
+import AddIcon from "@mui/icons-material/Add"
+
+import classes from "../components/content.classes"
+
+import constants from "../constants"
 
 const sortingAlgorithms = [
   "Bubble Sort",
@@ -20,10 +27,28 @@ const sortingAlgorithms = [
 
 const SortingAlgorithmsPage = () => {
   // Local state
-  const [selected, setSelected] = React.useState(-1)
+  const [selected, setSelected] = React.useState(0) // Show bubble first as default
   const [speed, setSpeed] = React.useState(500)
-  const [noOfEntries, setNoOfEntries] = React.useState(30)
-  const [data, setData] = React.useState(null)
+  const [noOfEntries, setNoOfEntries] = React.useState(25)
+  const [data, setData] = React.useState([])
+  const [running, setRunning] = React.useState(false)
+
+  const initialData = React.useRef(null)
+  const timeouts = React.useRef([])
+
+  // Constants
+  const [max, min] = React.useMemo(
+    () => [Math.max(...data), Math.min(...data)],
+    [data]
+  )
+  const calcWidth = 100 / data.length
+  const twoNumbersSelection = React.useRef([])
+  const sortedValues = React.useMemo(() => new Set(), [data])
+  // Do sort - call sorting algorithm
+  const sortingSteps = React.useMemo(
+    () => algos.bubbleSortAlgorithm(data),
+    [data]
+  )
 
   // Handlers
   const handleSelection = (event: any) => {
@@ -32,7 +57,7 @@ const SortingAlgorithmsPage = () => {
   }
 
   // Load random data
-  React.useEffect(() => {
+  const generateNewRandomData = () => {
     const newData = Array.from({ length: noOfEntries })
 
     let index = 0
@@ -43,22 +68,90 @@ const SortingAlgorithmsPage = () => {
         index++
       }
     }
+    initialData.current = newData.slice()
     setData(newData)
+  }
+  // Update data table with new values on entries change
+  React.useEffect(() => {
+    generateNewRandomData()
   }, [noOfEntries])
 
-  // Render selected algorithm
-  let selectedJSX = null
-  if (data) {
-    switch (selected) {
-      case sortingAlgorithms.indexOf("Bubble Sort"):
-        selectedJSX = <BubbleSort data={data} speed={speed} />
-        break
-      default:
-        break
+  // Start the sort
+  const handleSorting = () => {
+    setRunning(true)
+
+    const sortingSteps = algos.bubbleSortAlgorithm(data) // Bubble for now
+    sortingSteps.push([-1, -1, false, null, true])
+
+    for (let index = 0; index < sortingSteps.length; index++) {
+      const orderEl = sortingSteps[index]
+
+      // Destructure el
+      const [firstEl, secondEl, swap, sortedValue, lastEl]: number[] = orderEl
+
+      // Add to sorted Set if existing
+      if (sortedValue) sortedValues.add(sortedValue)
+
+      // Update state to trigger re-render. Do it with a timeout
+      setTimeout(() => {
+        if (lastEl) {
+          setRunning(false)
+          // Set local data to trigger a final render
+          setData((prevValues: number[]) => [...prevValues])
+          // Add the remaining elements to the values sorted array
+          sortingSteps?.forEach((el) => sortedValues.add(el))
+        } else {
+          setData((prevValues: number[]) => {
+            const indexOfFirst = prevValues.indexOf(firstEl)
+            const indexOfSecond = prevValues.indexOf(secondEl)
+
+            if (swap) {
+              prevValues[indexOfFirst] = secondEl
+              prevValues[indexOfSecond] = firstEl
+            }
+
+            const containerWidth =
+              document.getElementById("bars-container")?.clientWidth ||
+              window.innerWidth - 20 // Guessed offset
+
+            const offset = containerWidth / data.length
+
+            if (firstEl && secondEl) {
+              // Slide first bar
+              const leftEl = document.getElementById(`bar-${firstEl}`)
+              if (leftEl)
+                leftEl.style.transform = `translate(${
+                  firstEl > secondEl ? offset : 0
+                }px,0)`
+
+              // Slide second bar
+              const rightEl = document.getElementById(`bar-${secondEl}`)
+              if (rightEl)
+                rightEl.style.transform = `translate(${
+                  secondEl > firstEl ? 0 : -offset
+                }px,0)`
+            }
+
+            twoNumbersSelection.current = [
+              Math.min(firstEl, secondEl),
+              Math.max(firstEl, secondEl),
+            ]
+            // twoNumbersSelection.current[1] = secondEl
+            return prevValues.slice()
+          })
+        }
+      }, speed * (index + 1))
     }
   }
 
-  // console.log("speed", speed)
+  const handleStop = () => {
+    console.log("STOP")
+
+    setData((prevData) => prevData.slice())
+  }
+
+  console.log("running", running)
+  console.log("timeouts.current", timeouts.current)
 
   return (
     <Layout>
@@ -69,8 +162,6 @@ const SortingAlgorithmsPage = () => {
         selected={selected}
         setSpeed={setSpeed}
         speed={speed}
-        setNoOfEntries={setNoOfEntries}
-        noOfEntries={noOfEntries}
         title="Sorting Algorithms"
       >
         <div
@@ -82,48 +173,109 @@ const SortingAlgorithmsPage = () => {
         >
           <div
             style={{
-              marginTop: -5,
-              marginBottom: 5,
+              // marginTop: -5,
+              // marginBottom: 5,
               padding: 0,
               display: "flex",
               flexDirection: "row",
-              justifyContent: "space-around",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginBottom: 10,
             }}
           >
-            <Box width={300}>
-              <span style={{ fontSize: 14, color: "gray", marginLeft: 5 }}>
-                Entries
-              </span>
-              <Slider
-                aria-label="Entries"
-                defaultValue={noOfEntries}
-                onChange={(event) => setNoOfEntries(event.target.value)}
-                // getAriaValueText={noOfEntries}
-                marks
-                min={20}
-                max={60}
-                step={5}
-                style={{ marginTop: -5 }}
-                valueLabelDisplay="auto"
-              />
+            <Box
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                width: "30%",
+              }}
+            >
+              <div
+                style={{
+                  // marginLeft: 10,
+                  width: "100%",
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  color: "gray",
+                }}
+              >
+                <AddIcon style={{ marginRight: 7, fontSize: 18 }} />
+                <Slider
+                  aria-label="Entries"
+                  defaultValue={noOfEntries}
+                  onChange={(event) => setNoOfEntries(event.target.value)}
+                  // getAriaValueText={noOfEntries}
+                  // marks
+                  min={5}
+                  max={100}
+                  // step={5}
+                  valueLabelDisplay="auto"
+                />
+              </div>
+              <div
+                style={{
+                  marginLeft: 25,
+                  width: "100%",
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  color: "gray",
+                }}
+              >
+                <FastForwardIcon style={{ marginRight: 7, fontSize: 18 }} />
+                <Slider
+                  aria-label="Speed"
+                  defaultValue={speed}
+                  onChange={(event) => setSpeed(event.target.value)}
+                  // getAriaValueText={noOfEntries}
+                  min={25}
+                  max={1000}
+                  // step={50}
+                  valueLabelDisplay="auto"
+                />
+              </div>
             </Box>
-            <Box width={300}>
-              <span style={{ fontSize: 14, color: "gray" }}>Speed</span>
-              <Slider
-                aria-label="Speed"
-                defaultValue={speed}
-                onChange={(event) => setSpeed(event.target.value)}
-                // getAriaValueText={noOfEntries}
-                marks
-                min={25}
-                max={800}
-                step={50}
-                style={{ marginTop: -5 }}
-                valueLabelDisplay="auto"
-              />
+            <Box
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-end",
+                alignItems: "center",
+                width: "30%",
+              }}
+            >
+              {/* <span style={{ fontSize: 14, color: "gray" }}>Start</span> */}
+              <Button
+                onClick={generateNewRandomData}
+                style={
+                  {
+                    // marginBottom: 5,
+                  }
+                }
+                sx={{ ...classes.button }}
+              >
+                Randomize
+              </Button>
+              <Button
+                onClick={running === true ? handleStop : handleSorting}
+                style={{
+                  marginLeft: 10,
+                  marginRight: 5,
+                }}
+                sx={{ ...classes.button }}
+              >
+                {running === true ? "Stop" : "Start"}
+              </Button>
             </Box>
           </div>
-          {selectedJSX}
+          <SortingTable
+            data={data}
+            sortedValues={sortedValues}
+            twoNumbersSelection={twoNumbersSelection}
+          />
         </div>
       </Content>
     </Layout>
